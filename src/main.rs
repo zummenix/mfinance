0d74acc74c919f24ca9a1daa18e1603e1a595b89
Aleksey Kuznetsov <zummenix@gmail.com>
2025-02-18 20:24:46 +0500

    Add human readable formatting for decimal

--- src/main.rs
+++ src/main.rs
@@ -52,10 +52,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         Commands::Report { period, file } => {
             if let Some(period) = period {
-                let total = generate_report(&file, &period)?;
+                let total = generate_report(&file, &period)?.human_readable();
                 println!("Total amount for {period}: {total}");
             } else {
-                let total = generate_report_for_all(&file)?;
+                let total = generate_report_for_all(&file)?.human_readable();
                 println!("Total amount: {total}");
             }
         }
@@ -101,9 +101,9 @@ fn add_entry(
 
     let total_after: Decimal = records_from_file(file_path)?.iter().map(|r| r.amount).sum();
 
-    let total_before_line = format!("{total_before:.2}");
-    let diff_line = format!("{:.2}", (total_after - total_before));
-    let total_after_line = format!("Total: {total_after:.2}");
+    let total_before_line = total_before.human_readable();
+    let diff_line = (total_after - total_before).human_readable();
+    let total_after_line = format!("Total: {}", total_after.human_readable());
 
     let max_len = [&total_before_line, &diff_line, &total_after_line]
         .iter()
@@ -151,4 +151,143 @@ fn generate_report(file_path: &Path, period: &str) -> Result<Decimal, Box<dyn st
 fn generate_report_for_all(file_path: &Path) -> Result<Decimal, Box<dyn std::error::Error>> {
     let records = records_from_file(file_path)?;
     Ok(records.into_iter().map(|r| r.amount).sum())
+}
+
+trait HumanReadable {
+    fn human_readable(&self) -> String;
+}
+
+impl HumanReadable for Decimal {
+    fn human_readable(&self) -> String {
+        let precision: usize = 2;
+        let decimal = self.round_dp(precision as u32);
+        let decimal_string = format!("{decimal:.precision$}");
+        let sign_offset = usize::from(decimal.is_sign_negative());
+        let len_till_dot = decimal_string.len() - 1 - precision;
+        let mut group_separator_index = (len_till_dot - sign_offset) % 3 + sign_offset;
+        if group_separator_index == sign_offset {
+            group_separator_index = 3 + sign_offset;
+        }
+        let mut result = String::new();
+        for (i, ch) in decimal_string.char_indices() {
+            if group_separator_index == i && group_separator_index < len_till_dot {
+                result.push(' ');
+                group_separator_index += 3;
+            }
+            result.push(ch);
+        }
+
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rust_decimal::{prelude::FromPrimitive, Decimal};
+
+    use super::HumanReadable;
+
+    #[test]
+    fn format_fractions() {
+        assert_eq!(Decimal::from_f32(0.006).unwrap().human_readable(), "0.01");
+    }
+
+    #[test]
+    fn format_fractions_negative() {
+        assert_eq!(Decimal::from_f32(-0.006).unwrap().human_readable(), "-0.01");
+    }
+
+    #[test]
+    fn format_singles() {
+        assert_eq!(Decimal::from_i8(1).unwrap().human_readable(), "1.00");
+    }
+
+    #[test]
+    fn format_singles_negative() {
+        assert_eq!(Decimal::from_i8(-1).unwrap().human_readable(), "-1.00");
+    }
+
+    #[test]
+    fn format_tens() {
+        assert_eq!(Decimal::from_i8(10).unwrap().human_readable(), "10.00");
+    }
+
+    #[test]
+    fn format_tens_negative() {
+        assert_eq!(Decimal::from_i8(-10).unwrap().human_readable(), "-10.00");
+    }
+
+    #[test]
+    fn format_hundreds() {
+        assert_eq!(Decimal::from_i8(100).unwrap().human_readable(), "100.00");
+    }
+
+    #[test]
+    fn format_hundreds_negative() {
+        assert_eq!(Decimal::from_i8(-100).unwrap().human_readable(), "-100.00");
+    }
+
+    #[test]
+    fn format_thousands() {
+        assert_eq!(
+            Decimal::from_f32(1999.99).unwrap().human_readable(),
+            "1 999.99"
+        );
+    }
+
+    #[test]
+    fn format_thousands_negative() {
+        assert_eq!(
+            Decimal::from_f32(-1999.99).unwrap().human_readable(),
+            "-1 999.99"
+        );
+    }
+
+    #[test]
+    fn format_ten_thousands() {
+        assert_eq!(
+            Decimal::from_f32(19999.99).unwrap().human_readable(),
+            "19 999.99"
+        );
+    }
+
+    #[test]
+    fn format_ten_thousands_negative() {
+        assert_eq!(
+            Decimal::from_f32(-19999.99).unwrap().human_readable(),
+            "-19 999.99"
+        );
+    }
+
+    #[test]
+    fn format_hundred_thousands() {
+        assert_eq!(
+            Decimal::from_f64(199999.99).unwrap().human_readable(),
+            "199 999.99"
+        );
+    }
+
+    #[test]
+    fn format_hundred_thousands_negative() {
+        assert_eq!(
+            Decimal::from_f64(-199999.99).unwrap().human_readable(),
+            "-199 999.99"
+        );
+    }
+
+    #[test]
+    fn format_million() {
+        assert_eq!(
+            Decimal::from_f64(1999999.99).unwrap().human_readable(),
+            "1 999 999.99"
+        );
+    }
+
+    #[test]
+    fn format_million_negative() {
+        assert_eq!(
+            Decimal::from_f64(-1999999.99).unwrap().human_readable(),
+            "-1 999 999.99"
+        );
+    }
 }
